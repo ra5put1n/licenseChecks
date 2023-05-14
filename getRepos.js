ef3080043710f8358e0aa9f6d758a2452ff51e80
ra5put1n <kaipakrishna380@gmail.com>
2023-05-15 00:11:33 +0200

    Feat: Added Alibaba and Apache

--- getRepos.js
+++ getRepos.js
@@ -15,12 +15,12 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
 const uri = process.env.MONGODB_URI;
 
-async function getCurrentPage() 
+async function getCurrentPage(key = "currentPage") 
 {
 	const client = await MongoClient.connect(uri);
 	const db = client.db('jobs');
 	const settingsCollection = db.collection('settings');
-	const settings = await settingsCollection.findOne({ key: 'currentPage' });
+	const settings = await settingsCollection.findOne({ key: key });
 	const currentPage = settings.value;
 	client.close();
 	return currentPage;
@@ -33,6 +33,16 @@ async function updateCurrentPage(newPage)
 	const settingsCollection = db.collection('settings');
 	await settingsCollection.updateOne({ key: 'currentPage' }, { $set: { value: newPage } }, { upsert: true });
 	console.log("Updated current page to " + newPage + "\n")
+	client.close();
+}
+
+async function updateCurrentPage2(newPage) 
+{
+	const client = await MongoClient.connect(uri);
+	const db = client.db('jobs');
+	const settingsCollection = db.collection('settings');
+	await settingsCollection.updateOne({ key: 'currentPage2' }, { $set: { value: newPage } }, { upsert: true });
+	console.log("Updated current page2 to " + newPage + "\n")
 	client.close();
 }
 
@@ -50,17 +60,32 @@ export async function getRepoLicense(link)
 export async function getRepos() 
 {
 	const orgs = ["google","facebook","microsoft"];
+	const orgs2 = ["alibaba","apache"];
 	const sort = "stars";
 	const order = "desc";
 	const perPage = 5;
-	let page = await getCurrentPage();
+	let page1 = await getCurrentPage();
+	let page2 = await getCurrentPage("currentPage2");
 	let repos = [];
 
 	for(const org of orgs)
 	{
 		// Fetch up to maxPages pages of results
 		const response = await octokit.request(`GET /orgs/${org}/repos`, {
-			page: page,
+			page: page1,
+			per_page: perPage
+		});
+		for(let i = 0; i < response.data.length; i++)
+		{
+			repos.push(response.data[i]);
+		}
+	}
+
+	for(const org of orgs2)
+	{
+		// Fetch up to maxPages pages of results
+		const response = await octokit.request(`GET /orgs/${org}/repos`, {
+			page: page2,
 			per_page: perPage
 		});
 		for(let i = 0; i < response.data.length; i++)
@@ -69,14 +94,20 @@ export async function getRepos()
 		}
 	}
 	// Update the result count and page counter
-	page += 1;
-	await updateCurrentPage(page);
+	page1 += 1;
+	page2 += 1;
+	await updateCurrentPage(page1);
+	await updateCurrentPage2(page2);
+	// console.log("Current page: " + page1);
+	// console.log("Current page2: " + page2);
 
 	// Return the links of the top 10 starred repositories
-	return repos.slice(0, 15).map((repo) => repo.html_url);
+	return repos.slice(0, 25).map((repo) => repo.html_url);
 }
 // For cleanup and testing
 // await updateCurrentPage(1);
+// let page2 = await getCurrentPage("currentPage");
+// console.log(page2);
 // let res = await getRepos();
 // console.log(res);
 
